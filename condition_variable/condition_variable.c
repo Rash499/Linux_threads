645082dedfe8e28120a0ffa17e0fb8d3150745a8
Rash499 <rashmikadilmin499@gmail.com>
2025-01-05 23:44:09 +0530

    simulate car fueling using threads

---
+++ condition_variable/condition_variable.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <error.h>
+
+pthread_mutex_t mutexFuel;
+int fuel = 0;
+
+void* fuel_filling(void* arg){
+
+    for(int i = 0; i < 5; i++){
+        pthread_mutex_lock(&mutexFuel);
+        fuel += 15;
+        printf("Filled fuel... %d\n",fuel);
+        pthread_mutex_unlock(&mutexFuel);
+        sleep(1);
+    }
+}
+
+void* car(void* arg){
+    pthread_mutex_lock(&mutexFuel);
+    fuel -= 40;
+    printf("Got fuel. Now left: %d\n",fuel);
+    
+    pthread_mutex_unlock(&mutexFuel);
+}
+
+int main(int argc, char* argv[]){
+
+    pthread_t th[2];
+
+    pthread_mutex_init(&mutexFuel, NULL);
+    for(int i = 0; i < 2; i++){
+        if(i== 1){
+            if(pthread_create(&th[i], NULL, &fuel_filling, NULL) != 0){
+                perror("Failed to creating thread");
+            }
+        }
+        else{
+                if(pthread_create(&th[i], NULL, &car, NULL) != 0){
+                    perror("Failed to creating thread");
+                }
+        }
+            
+        
+    }
+
+    for(int i = 0; i< 2; i++){
+        if(pthread_join(th[i],NULL) != 0){
+            perror("Failed to join thread");
+        }
+    }
+
+    pthread_mutex_destroy(&mutexFuel);
+    
+    return 0;
+}
